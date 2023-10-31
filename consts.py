a27344afb855b3fd2509cfb5bb55cd0bca7d47df
Kyeongman-header <zzangman2@gmail.com>
2023-10-31 18:33:52 +0900

    consts

--- consts.py
+++ consts.py
@@ -15,5 +15,5 @@ canvas.pack()
 # nucleobase=[[10,0],[0,10],[-10,0],[0,-10]] # 핵염기. AGCT. 동서남북
 nucleobase=[[10,10],[-10,10],[-10,-10],[10,-10]] # 핵염기. AGCT. 동서, 서남, 남동, 동북
 
-whole_generations=3
-whole_individuals=5
+whole_generations=100
+whole_individuals=1000
